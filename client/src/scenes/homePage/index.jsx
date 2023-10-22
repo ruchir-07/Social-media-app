46a57b54f72bf293e4f1e6ad519c8a45b8cdaade
ruchir-07 <99608006+ruchir-07@users.noreply.github.com>
2023-10-23 02:11:14 +0530

    login done

---
+++ client/src/scenes/homePage/index.jsx
@@ -0,0 +1,14 @@
+import Navbar from "scenes/navbar"
+import React from "react";
+
+const { Box } = require("@mui/material")
+
+const HomePage = () => {
+    return(
+        <Box>
+            <Navbar />
+        </Box>
+    );
+}
+
+export default HomePage
